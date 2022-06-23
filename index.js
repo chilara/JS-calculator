731e17f4dd9b6e71e1ff0438530fbd7a96fc4421
Chisom Oguine <oguinechisomtimothy@gmail.com>
2022-06-22 23:15:38 -0700

    modified js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const num1 = prompt("enter first number")
+const opr = prompt("enter mathematical operator")
+const num2 = prompt("enter second number")
+
+if(opr == "+")
+
+{
+    alert(Number(num1) + Number(num2))
+}
+else if(opr == "-")
+{
+    alert(Number(num1) - Number(num2))
+}
+else if(opr == "*")
+{
+    alert(Number(num1) * Number(num2))
+}
+else if(opr == "/")
+{
+    alert(Number(num1) / Number(num2))
+} 
